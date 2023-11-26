87b6cda592b8f3a4462bd7b809f728d23df38e90
Milton3000 <kristofferssonmilton@gmail.com>
2023-11-26 21:55:52 +0100

    Extra changes visibility

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ function App() {
   return (
     <div className="App">
       <Regions />
-      <CountryList />
     </div>
   );
 }
--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -1,17 +1,13 @@
 
-
 import React from "react";
 
 const CountryList = ({ regionData }) => {
-  if (!regionData || !Array.isArray(regionData)) {
-    return <p>No data available.</p>;
-  }
 
   return (
     <div>
       <h1> Countries: </h1>
       <ul>
-        {regionData.map((country, index) => (
+        {regionData && regionData.map((country, index) => (
           <li key={index}>
             {country.name?.common} - {country.capital} - Population:{" "}
             {country.population}
